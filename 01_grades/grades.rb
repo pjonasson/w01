d48d683538c26c5dec254d01f18b6ea36a44d13e
Percy Jonasson <pjonasson@outlook.com>
2021-09-09 12:17:56 -0500

    Completed Grades exercise

--- 01_grades/grades.rb
+++ 01_grades/grades.rb
@@ -1,6 +1,18 @@
 def final_grade(array)
-
-  # This method currently has no code.
-  # Your job is to write the code to get the method to work!
-
+  if array == []
+    return "I"
+  elsif sum = array.reduce(:+)
+    mean = (sum / array.length).round
+    if mean >= 90
+      return "A"
+    elsif mean >= 80 && mean <= 89
+      return "B"
+    elsif mean >= 70 && mean <= 79
+      return "C"
+    elsif mean >= 60 && mean <= 69
+      return "D"
+    elsif mean < 60
+      return "F"
+    end
+  end
 end
